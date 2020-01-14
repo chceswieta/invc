8a4df78742b83008cd4d6da8aa4af7714b53259e
GabrielWechta <wechtagabriel@gmail.com>
2020-01-14 18:40:33 +0100

    one of the procedure done

--- src/main/procedures.sql
+++ src/main/procedures.sql
@@ -1,12 +1,14 @@
 USE invc;
 
-DROP PROCEDURE IF EXISTS generateInvoiceHeadline;
+#DROP PROCEDURE IF EXISTS generateInvoiceHeadline;
+DROP PROCEDURE IF EXISTS gen;
 
 DELIMITER $$
 
-CREATE PROCEDURE generateInvoiceHeadline(IN invoiceId INT)
+#CREATE PROCEDURE generateInvoiceHeadline(IN invoiceId INT)
+CREATE PROCEDURE gen(IN invoiceId INT)
 BEGIN		
-	SELECT CONCAT('invcId:', invoiceId, ' ,' FROM invoice WHERE invoice.invoiceId = invoiceId;
+	SELECT CONCAT(clientId, ' ', invoiceId, ' ', date, ' ', total) FROM invoice WHERE invoice.invoiceId = invoiceId;
 END $$
 
 DELIMITER ;
