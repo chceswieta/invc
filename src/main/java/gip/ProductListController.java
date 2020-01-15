9fad7d9fb23b2b8374f4bdb3c3193fe440db2584
chceswieta <chceswieta@gmail.com>
2020-01-15 23:06:10 +0100

    Make product choice box smarter and cuter.

--- src/main/java/gip/ProductListController.java
+++ src/main/java/gip/ProductListController.java
@@ -4,17 +4,22 @@ import javafx.scene.control.ChoiceBox;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
 
 public abstract class ProductListController extends ResponseController {
     public ChoiceBox<String> product;
+    private LinkedHashMap<String, Integer> products = new LinkedHashMap<>();
 
     public void initialize() throws SQLException {
-        ResultSet rs = executeQuery("SELECT productId, productName FROM product");
-        while (rs.next()) product.getItems().add(rs.getString(1) + " " + rs.getString(2));
+        ResultSet rs = executeQuery("SELECT productId, productName FROM product ORDER BY productId");
+        while (rs.next()) {
+            product.getItems().add(rs.getString(2));
+            products.put(rs.getString(2), rs.getInt(1));
+        }
     }
 
     public int getProductId() {
-        return Integer.parseInt(product.getValue().split(" ")[0]);
+        return products.get(product.getValue());
     }
 
     public boolean productChosen() {
