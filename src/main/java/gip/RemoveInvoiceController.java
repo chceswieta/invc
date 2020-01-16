4c426adc8663ea97e25164b7b9b49d8e86136a9e
GabrielWechta <wechtagabriel@gmail.com>
2020-01-16 11:36:48 +0100

    quick commit for fixing int spinner

--- src/main/java/gip/RemoveInvoiceController.java
+++ src/main/java/gip/RemoveInvoiceController.java
@@ -1,11 +1,30 @@
 package gip;
 
-import javafx.scene.control.Label;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javafx.scene.control.TextField;
 
 public class RemoveInvoiceController extends ResponseController {
-    public TextField invoiceID;
+	public TextField invoiceID;
 
-    public void removeInvoice() {
-    }
+	public void removeInvoice() {
+		resetFocus();
+		if (invoiceID.getText().isEmpty()) {
+			redFocus(invoiceID);
+			setInfo("Field cannot be empty.", true);
+		} else {
+			try {
+                PreparedStatement preparedStatement = prepareStatement("DELETE FROM invoice WHERE invoiceId = ?");
+                preparedStatement.setString(1, invoiceID.getText());
+                int affected = preparedStatement.executeUpdate();
+                if (affected == 1) setInfo("Done.", false);
+                else if (affected == 0) setInfo("Invoice not found.", true);
+                else setInfo("Something went wrong.", true);
+                invoiceID.clear();
+			} catch (SQLException e) {
+				setInfo(e.getMessage(), true);
+			}
+		}
+	}
 }
